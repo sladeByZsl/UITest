5b9000ed22915a529d1be7c94b3d185c4c7aefa0
silai.zhou <silai.zhou@puzzleplusgames.com>
2021-04-07 14:38:13 +0800

    1

---
+++ Assets/Scenes/ClassTem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Enemy
+{
+    [SerializeField]
+    string name;
+
+    [SerializeField]
+    List<string> skills;
+    public Enemy(string name, List<string> skills)
+    {
+        this.name = name;
+        this.skills = skills;
+    }
+}
+
+public class EnemyWrap
+{
+    public List<Enemy> enemies = new List<Enemy>();
+    public EnemyWrap()
+    {
+        
+    }
+}
